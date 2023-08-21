8b99537cd085f4d1259616175890a781b83b7be2
ZulfiPy <zulfizupy@gmail.com>
2023-08-22 00:38:08 +0300

    create final dataframes for further plot

---
+++ generate_final_dfs_for_plot.py
@@ -0,0 +1,20 @@
+from preprocess_dfs import Preprocess
+import os
+
+class Generator:
+    def __init__(self):
+        dir_path = '/Users/zulf/Documents/GitHub/div/data'
+        self.dir_dfs = os.listdir(dir_path)
+
+    def dfs_for_plot(self, df_num):
+        mean_count_df = Preprocess(self.dir_dfs[df_num]).df_mean_count()
+        div_sum_df = Preprocess(self.dir_dfs[df_num]).df_div_sum()
+        location_df = Preprocess(self.dir_dfs[df_num]).df_location()
+
+        return {
+            'mean_count_df': mean_count_df,
+            'div_sum_df': div_sum_df,
+            'location_df': location_df
+        }
+
+generate = Generator()
