64f6a8d052a35d8dee22b14290e65edb84bcddb0
ZulfiPy <zulfizupy@gmail.com>
2023-08-26 17:34:52 +0300

    create yearly df of dividend yield

--- create_yearly_df_of_div_prc.py
+++ create_yearly_df_of_div_prc.py
@@ -11,12 +11,27 @@ class DataFrameCreator:
         self.columns = [column for column in generate.dfs_for_plot(0)['mean_count_df']['industry']]
 
         self.df = None
+        self.temp_data = None
 
     def create_df(self):
         self.df = pd.DataFrame(data=None, index=range(len(self.year_indexes)), columns=self.columns)
         self.df.insert(loc=0,
                        column='Year',
                        value=self.year_indexes)
+        return self.df
+
+    def fill_df(self, df_num):
+        exdf = generate.dfs_for_plot(self.df_position[df_num])['mean_count_df']
+        for idx in range(len(self.df.columns[1:])):
+            for i in range(len(exdf)):
+                if exdf.iloc[i, 0] == self.df.columns[idx + 1]:
+                    self.df.iloc[df_num, idx + 1] = exdf.iloc[i, 1]
+        return self.df
+
+    def run_create_fill(self):
+        self.create_df()
+        for index, value in self.df.iterrows():
+            self.fill_df(index)
         return self.df
 
 df_creator = DataFrameCreator()
